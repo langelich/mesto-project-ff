72647359bc15e0ccb506316153f0cb69c7e83dcf
Алексей Ангелич <l.angelich@yandex.ru>
2024-06-12 17:30:15 +0400

    js added

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,37 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document.querySelector('#card-template').content;
+
 // @todo: DOM узлы
+
+const mainContent = document.querySelector('.content');
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
 
+function addCard(elem, deleteCard) {
+    const placesItem = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const deleteButton = placesItem.querySelector('.card__delete-button');
+
+    placesItem.querySelector('.card__image').src = elem.link;
+    placesItem.querySelector('.card__title').textContent = elem.name;
+    placesItem.querySelector('.card__image').alt = elem.name;
+    
+    deleteButton.addEventListener('click', deleteCard);
+
+    return placesItem;
+};
+
 // @todo: Функция удаления карточки
 
+function deleteCard(evt) {
+    const cardDeleted = evt.target.closest('.places__item');
+    cardDeleted.remove();
+};
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach(function (elem) {
+    const card = addCard(elem, deleteCard);
+    placesList.append(card);
+})
